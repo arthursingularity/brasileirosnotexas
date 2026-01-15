1bf225f1aa51d35dca48697833ee63e4b7bb5368
Arthur <arthur.1234.5678.alves@gmail.com>
2026-01-15 20:52:07 -0300

    code updated!

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,25 @@ export const metadata = {
     default: "Comunidade Brasileiros no Texas"
   },
   description: "Realize o seu sonho de morar nos Estados Unidos",
+  openGraph: {
+    title: "Comunidade Brasileiros no Texas",
+    description:
+      "Realize o seu sonho de morar nos Estados Unidos",
+    url: "https://brasileirosnotexas.vercel.app/",
+    siteName: "Comunidade Brasileiros no Texas",
+    images: [
+      {
+        url: "https://brasileirosnotexas.vercel.app/og-image.jpg",
+        width: 800,
+        height: 800,
+        alt: "Comunidade Brasileiros no Texas",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
